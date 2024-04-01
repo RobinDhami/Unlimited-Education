eea287871af543d4c3bd6961affcf7f938f97742
RobinDhami <rozendhami69@gmail.com>
2024-04-01 16:02:18 +0545

    search button added

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -7,8 +7,8 @@ function Navbar() {
   };
 
   return (
-    <header className="bg-black rounded-sm border-none shadow-slate-200">
-      <nav className=" flex justify-between items-center w-[92%] mx-auto lg:h-20 h-20 ">
+    <header className="bg-black rounded-sm border-none shadow-sm shadow-[black] ">
+      <nav className=" flex justify-between items-center w-[92%] mx-auto lg:h-20 h-20 ml-5">
         <div>
           <img
             className="w-16 cursor-pointer"
@@ -16,6 +16,17 @@ function Navbar() {
             
           />
         </div>
+                <div className="hidden lg:block flex items-centern ">
+          <input
+            type="text"
+            placeholder="Search..."
+            className="bg-white text-black px-4 py-2 rounded-md focus:outline-none focus:ring focus:border-blue-400"
+          />
+          <button className="ml-2 bg-[#a6c1ee] text-white px-4 py-2 rounded-md hover:bg-[#87acec]">
+            Search
+          </button>
+        </div>
+
         <div
           className={`nav-links duration-500 md:static absolute  md:min-h-fit min-h-[60vh] 
           left-0 ${isMenuOpen ? 'top-[9%]' : 'top-[-100%]'} md:w-auto w-full flex items-center px-5`}
