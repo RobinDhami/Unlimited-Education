82afe390327bfc993f63375dbbf938db4b8fefdf
RobinDhami <rozendhami69@gmail.com>
2024-04-04 13:19:30 +0545

    footer

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Hero from "./Components/herosection/herosection"
 import Service from "./Components/Services/service"
 import About1 from "./Components/About/About"
 import Contact from "./Components/Contact/Contact"
+import { CardFooter } from "@material-tailwind/react"
 function App() {
   return (
 <div>
@@ -11,6 +12,7 @@ function App() {
 <Service/>
 <About1/>
 <Contact/>
+<CardFooter/>
 </div>)
 }
 
